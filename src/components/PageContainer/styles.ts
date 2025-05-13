ba304f824b34710e0da02d2d953aa57ed89f899c
Joshua San Jose <forimbajosh21@gmail.com>
2025-05-13 23:10:10 +0800

    chore: Create PageContainer component

---
+++ src/components/PageContainer/styles.ts
@@ -0,0 +1,12 @@
+import { Colors } from '@/src/constants/Colors';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.light.background.main,
+  },
+});
+
+export default styles;
