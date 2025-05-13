1e98e3f2f09329b00fd285431a7deff076eac892
Joshua San Jose <forimbajosh21@gmail.com>
2025-05-13 23:11:24 +0800

    chore: Create MovieList component to display a horizontal list of movies

---
+++ src/components/MovieList/index.tsx
@@ -0,0 +1,48 @@
+import { FlatList, View } from 'react-native';
+
+import Text from '@/src/components/Text';
+
+import { Movie } from '@/src/types/Movie';
+import MovieCard from '../MovieCard';
+
+export interface MovieListProps {
+  list: Movie[];
+}
+
+const MovieList: React.FC<MovieListProps> = ({ list }) => {
+  return (
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      data={list}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <MovieCard
+          title={item.title}
+          releaseDate={item.release_date}
+          posterPath={item.poster_path}
+        />
+      )}
+      ItemSeparatorComponent={() => (
+        <View
+          style={{
+            width: 24,
+          }}
+        />
+      )}
+      ListEmptyComponent={() => (
+        <Text variant="body" color="secondary">
+          No movies found
+        </Text>
+      )}
+      contentContainerStyle={{
+        paddingHorizontal: 16,
+      }}
+      style={{
+        marginTop: 8,
+      }}
+    />
+  );
+};
+
+export default MovieList;
